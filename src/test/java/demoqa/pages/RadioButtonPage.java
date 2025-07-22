2ec625fbf72510f40b3b6f5c27cab3af52c64999
GorskyAlexander <zuusas@gmail.com>
2025-07-22 14:13:14 +0200

    fix typo

--- src/test/java/demoqa/pages/RadioButtonPage.java
+++ src/test/java/demoqa/pages/RadioButtonPage.java
@@ -11,7 +11,7 @@ public class RadioButtonPage extends BasePage {
     private final BaseElement yesRadio = new BaseElement(driver, By.id("yesRadio"));
     private final BaseElement impressiveRadio = new BaseElement(driver, By.id("impressiveRadio"));
     private final BaseElement resultText = new BaseElement(driver, By.className("text-success"));
-    private final BaseElement radioButtonSelector = new BaseElement(driver, By.id("item-1"));
+    private final BaseElement radioButtonSelector = new BaseElement(driver, By.id("item-2"));
 
     public RadioButtonPage() {
         super(WebDriverSingleton.driver);
